2c5acabfd37b457c393c978163f24f201b41de40
Amnfn <134054440+Amnfn@users.noreply.github.com>
2023-07-22 11:06:11 +0200

    error occur

---
+++ lab-vue-basics/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import NavBar from './components/icons/NavBar.vue'
+import Footer from './components/icons/Footer.vue'
+
+
+</script>
+
+<template>
+  <NavBar> </NavBar>
+
+  <p>Shops are for adults only. Validate you're <input type="number" placeholder="age" {{ age }}> to enjoy or ask you're mom to help you find the best clothing for you!</p>
+  <p v-if="age >=14"> It's shopping day! Enjoy our limited edition with a reasonable price!</p>
+  <p v-else>It's too early for you to shop.</p>
+
+<!--- 
+<h2>Shops Here</h2> 
+  <ul>
+    <li>Men</li>
+    <li>Women</li>
+    <li>Kids</li>
+  </ul>
+  -->
+  <Footer> </Footer>
+  
+</template>
+
+
+<style>
+
+</style>
---
+++ lab-vue-basics/src/components/icons/NavBar.vue
@@ -0,0 +1,22 @@
+<script setup>
+const navbar = ["Account", "Home", "Shop", "Service Center"]
+</script>
+
+<template>
+    <nav class="navbar">
+        <ul class="nav-list">
+            <li v-for="nav in navbar">
+                {{ nav }}
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<style>
+.nav-list {
+    list-style-type: none;
+    display: flex;
+    justify-content: space-around;
+}
+
+</style>
